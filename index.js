7a568dc77c5d9021045bda824efeec9034c7c2b1
yasminhernandes <yasminhernandesjobs@gmail.com>
2022-02-04 00:47:50 -0300

    Create modal items added to cart

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const minusButton = document.querySelector(".minus-button")
 const plusButton = document.querySelector(".plus-button")
 const counterValue = document.querySelector(".value")
-const addCart = document.querySelector("add-to-cart")
 
 counterButtonHandler = operation => {
     let currentValue = +counterValue.innerHTML
@@ -21,15 +20,35 @@ counterButtonHandler = operation => {
         default:
             break;
     }
-
-    return counterValue
 }
-
-getCounterValue = 
 
 minusButton.addEventListener('click', () => counterButtonHandler("-"))
 plusButton.addEventListener('click', () => counterButtonHandler("+"))
 
-addCart.addEventListener('click',() => {
-    
-} )
+
+/* MODAL ADD TO CART  */
+const modalAddToCart = document.querySelector(".modal.add-cart")
+const buttonAddToCart = document.querySelector(".modal.add-cart.ok")
+const addToCart = document.querySelector(".add-to-cart")
+const bagItems = document.querySelector(".bag-items")
+const header = document.querySelector('header')
+const main = document.querySelector('main')
+const body = document.querySelector('body')
+
+openModal = () => {
+    header.style.opacity = "0.5"
+    main.style.opacity = "0.5"
+    modalAddToCart.classList.add('on')
+    bagItems.innerHTML = counterValue.innerHTML;
+    body.style.overflow= "hidden"
+}
+
+ closeModal = () => {
+    header.style.opacity = "1"
+    main.style.opacity = "1"
+    modalAddToCart.classList.remove('on')
+    body.style.overflowY = "initial"
+}
+
+window.setInterval(closeModal, 2500)
+addToCart.addEventListener('click', () => openModal())
